dac1e9fb0231d2eb6d9fcfdd02bbbaebdac66b5b
BryceEvans <evans.bryce@gmail.com>
2019-07-23 17:15:06 -0600

    Initial file structure created.

---
+++ server/api/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const cors = require('cors');
+
+const server = express();
+
+server.use(express.json());
+server.use(cors('*'));
+
+server.get('/', (req, res) => {
+    res.status(200).json({ server: "Server up and running!" });
+})
